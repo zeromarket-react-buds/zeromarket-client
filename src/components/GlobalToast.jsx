0089ffb25ead992b04dc165496a40248eefbe642
kimej214 <kimej21400@gmail.com>
2025-11-25 11:00:51 +0900

    [PL-01-01] 찜 목록 삭제 토스트 알림 추가, 주석 추가

--- src/components/GlobalToast.jsx
+++ src/components/GlobalToast.jsx
@@ -1,7 +1,8 @@
 import { ToastContainer, toast } from "react-toastify";
 
+// 토스트 함수
 export const useLikeToast = () => {
-  const showLikeToast = () => {
+  const showLikeAddedToast = () => {
     toast.success("찜 목록에 추가되었어요!", {
       position: "bottom-center",
       autoClose: 3000,
@@ -10,8 +11,18 @@ export const useLikeToast = () => {
     });
   };
 
-  return { showLikeToast };
+  const showLikeRemovedToast = () => {
+    toast.info("찜 목록에서 삭제했어요.", {
+      position: "bottom-center",
+      autoClose: 3000,
+      hideProgressBar: true,
+      pauseOnHover: false,
+    });
+  };
+  return { showLikeAddedToast, showLikeRemovedToast };
 };
+
+// 이후 다른쪽에서 필요한 토스트 알림 추가시에는 여기에 새롭게 해당 기능 export const 함수 추가
 
 export const GlobalToast = () => {
   return <ToastContainer />;
