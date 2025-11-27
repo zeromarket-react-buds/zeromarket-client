0715307b9cedbaca6583df636d324836121e2319
kimej214 <kimej21400@gmail.com>
2025-11-27 19:50:28 +0900

    [공통] 하위 컴포넌트에서 마이페이지 본문이 보이는 레이아웃 문제 해결

--- src/layouts/header/TitleHeader.jsx
+++ src/layouts/header/TitleHeader.jsx
@@ -1,7 +1,7 @@
 import { ChevronLeft, Bell } from "lucide-react";
 import Container from "@/components/Container";
-//rightSlot추가
-const TitleHeader = ({ title, rightSlot }) => {
+//rightSlot, hideRight추가
+const TitleHeader = ({ title, rightSlot, hideRight = false }) => {
   return (
     <Container>
       <header className="flex items-center justify-between mb-6">
@@ -10,7 +10,12 @@ const TitleHeader = ({ title, rightSlot }) => {
         </button>
         <h1 className="text-xl font-bold">{title}</h1>
         {/* 오른쪽 아이콘: 기본 Bell, 커스텀(rightSlot) 있으면 대체 {rightSlot ? rightSlot :}추가 */}
-        <button>{rightSlot ? rightSlot : <Bell size={24} />}</button>
+        {/* 오른쪽 아예 없는 경우 추가  */}
+        {hideRight ? (
+          <div className="w-6" /> // 자리는 맞추되 내용 없음
+        ) : (
+          <button>{rightSlot ? rightSlot : <Bell size={24} />}</button>
+        )}
       </header>
     </Container>
   );
