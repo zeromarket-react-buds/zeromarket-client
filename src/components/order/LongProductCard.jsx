3744b7353dbeb95a3ed3e26e83c15d38c49ec1fb
kimej214 <kimej21400@gmail.com>
2025-12-01 10:32:03 +0900

    [TR-01-01, TR-01-09] 사진 선택시 해당 상품상세 이동

--- src/components/order/LongProductCard.jsx
+++ src/components/order/LongProductCard.jsx
@@ -1,18 +1,27 @@
 import { Badge } from "@/components/ui/badge";
+import { useNavigate } from "react-router-dom";
 
 const LongProductCard = ({
+  productId,
   productTitle,
   sellPrice,
   tradeType,
   tradeStatus,
   thumbnailUrl,
 }) => {
+  const navigate = useNavigate();
+
+  const goDetail = (e) => {
+    e.stopPropagation();
+    navigate(`/products/${productId}`);
+  };
   return (
     <div className="flex flex-row gap-10 items-center">
       <div className="overflow-hidden">
         <img
           src={thumbnailUrl}
           className="n w-[140px] h-[100px] object-cover rounded-2xl"
+          onClick={goDetail}
         />
       </div>
       <div className="flex flex-col gap-1 flex-1">
@@ -20,7 +29,11 @@ const LongProductCard = ({
         <div className="font-semibold">{sellPrice.toLocaleString()}원</div>
         <div className="flex w-full flex-row items-center justify-between">
           <div className="text-brand-mediumgray">{tradeType}</div>
-          <Badge>{tradeStatus}</Badge>
+          {tradeStatus === "취소" ? (
+            <Badge variant="red">구매취소</Badge>
+          ) : (
+            <Badge>{tradeStatus}</Badge>
+          )}
         </div>
       </div>
     </div>
