458404fd0f1126643e9fe93a9e50a236bf685f61
kimej214 <kimej21400@gmail.com>
2025-12-03 17:40:39 +0900

    [TR-01-01, TR-01-09] godetail 삭제, 뱃지부분 구매취소 -> 거래취소로 바꿈

--- src/components/order/LongProductCard.jsx
+++ src/components/order/LongProductCard.jsx
@@ -1,27 +1,18 @@
 import { Badge } from "@/components/ui/badge";
-import { useNavigate } from "react-router-dom";
 
 const LongProductCard = ({
-  productId,
   productTitle,
   sellPrice,
   tradeType,
   tradeStatus,
   thumbnailUrl,
 }) => {
-  const navigate = useNavigate();
-
-  const goDetail = (e) => {
-    e.stopPropagation();
-    navigate(`/products/${productId}`);
-  };
   return (
     <div className="flex flex-row gap-10 items-center">
       <div className="overflow-hidden">
         <img
           src={thumbnailUrl}
-          className="n w-[140px] h-[100px] object-cover rounded-2xl"
-          onClick={goDetail}
+          className="w-[140px] h-[100px] object-cover rounded-2xl"
         />
       </div>
       <div className="flex flex-col gap-1 flex-1">
@@ -30,7 +21,7 @@ const LongProductCard = ({
         <div className="flex w-full flex-row items-center justify-between">
           <div className="text-brand-mediumgray">{tradeType}</div>
           {tradeStatus === "취소" ? (
-            <Badge variant="red">구매취소</Badge>
+            <Badge variant="red">거래취소</Badge>
           ) : (
             <Badge>{tradeStatus}</Badge>
           )}
