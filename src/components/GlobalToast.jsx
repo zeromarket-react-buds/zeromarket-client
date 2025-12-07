26d68ac2357d041a75702e7d296273009e833303
kimej214 <kimej21400@gmail.com>
2025-12-08 01:06:36 +0900

    [TR-01-05, TR-01-08, TR-01-13] 각 거래 변경, 소프트 딜리트시 토스트 알림 추가

--- src/components/GlobalToast.jsx
+++ src/components/GlobalToast.jsx
@@ -1,6 +1,6 @@
 import { ToastContainer, toast } from "react-toastify";
 
-// 토스트 함수
+// 찜 토스트 함수
 export const useLikeToast = () => {
   const showLikeAddedToast = () => {
     toast.success("찜 목록에 추가되었어요!", {
@@ -23,6 +23,42 @@ export const useLikeToast = () => {
 };
 
 // 이후 다른쪽에서 필요한 토스트 알림 추가시에는 여기에 새롭게 해당 기능 export const 함수 추가
+
+// 거래 토스트 함수
+export const useTradeToast = () => {
+  const showCompletedUpdatedToast = () => {
+    toast.success("거래 완료되었습니다", {
+      position: "bottom-center",
+      autoClose: 3000,
+      hideProgressBar: true,
+      pauseOnHover: false,
+    });
+  };
+
+  const showCanceledUpdatedToast = () => {
+    toast.info("거래 취소되었습니다.", {
+      position: "bottom-center",
+      autoClose: 3000,
+      hideProgressBar: true,
+      pauseOnHover: false,
+    });
+  };
+
+  const showSoftDeletedToast = () => {
+    toast.info("거래 내역이 삭제되었습니다.", {
+      position: "bottom-center",
+      autoClose: 3000,
+      hideProgressBar: true,
+      pauseOnHover: false,
+    });
+  };
+
+  return {
+    showCompletedUpdatedToast,
+    showCanceledUpdatedToast,
+    showSoftDeletedToast,
+  };
+};
 
 export const GlobalToast = () => {
   return <ToastContainer />;
