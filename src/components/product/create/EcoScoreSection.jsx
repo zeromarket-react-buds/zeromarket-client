b74897b3e48a386b818a6e78346b4fd72abed2da
bokku <ddgag38@gmail.com>
2025-12-11 16:03:26 +0900

    [PD-01-01] 상품상세, 하단버튼 수정 및 컴포넌트 분리

--- src/components/product/create/EcoScoreSection.jsx
+++ src/components/product/create/EcoScoreSection.jsx
@@ -1,12 +1,16 @@
+import { Button } from "@/components/ui/button";
 const EcoScoreSection = () => {
   return (
     <div className="mt-8 mb-20">
       {/* 상품등록화면 환경 점수 - 2,3차 개발*/}
       <p className="font-bold py-3 border-b text-lg">환경 점수</p>
-      <div className="w-full bg-brand-green text-white p-3 my-5 flex justify-between rounded-lg">
+      <Button
+        className="w-full p-3 py-6 my-5 flex justify-between rounded-lg text-md font-white "
+        variant="green"
+      >
         <span>환경점수</span>
         <span>100p</span>
-      </div>
+      </Button>
     </div>
   );
 };
