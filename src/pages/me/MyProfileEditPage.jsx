e7499a820c6256dfae48f26695c5cc5d58554e55
kimej214 <kimej21400@gmail.com>
2025-12-12 12:00:54 +0900

    [MY-01-05, TR-01-04, TR-01-12] 연락처 포매팅 함수 분리

--- src/pages/me/MyProfileEditPage.jsx
+++ src/pages/me/MyProfileEditPage.jsx
@@ -4,25 +4,7 @@ import { getProfileEditApi } from "@/common/api/profile.api";
 import { useEffect, useState } from "react";
 import PhoneEditModal from "@/components/profile/PhoneEditModal";
 import EmailEditModal from "@/components/profile/EmailEditModal";
-
-function formatPhone(phone = "") {
-  if (!phone) {
-    return ""; // 핸드폰번호 없을때 방어로직 (실제로 안들어오면 오류로 안 그려짐)
-  }
-
-  // 숫자만 추출
-  const digits = phone.replace(/\D/g, "");
-
-  if (digits.length === 11) {
-    // 010-1234-5678
-    return digits.replace(/(\d{3})(\d{4})(\d{4})/, "$1-$2-$3");
-  }
-
-  if (digits.length === 10) {
-    // 010-123-4567
-    return digits.replace(/(\d{3})(\d{3})(\d{4})/, "$1-$2-$3");
-  }
-}
+import formatPhone from "@/utils/formatPhone";
 
 const MyProfileEditPage = () => {
   // 현재 값
