1fad3e03b516ceb8f8b7eb2ad356fb39e773063c
bokku <ddgag38@gmail.com>
2025-12-24 14:35:21 +0900

    [PD-01-01] 신뢰점수 설명 말풍선 추가
    직전 커밋시 누락하여 재 푸쉬

--- src/components/product/detail/ProductSellerInfo.jsx
+++ src/components/product/detail/ProductSellerInfo.jsx
@@ -1,15 +1,16 @@
 import { UserRound, Heart, Eye, Smile } from "lucide-react";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const ProductSellerInfo = ({ detail }) => {
-  // const profileImage = detail.seller?.profileImage;
-  // const sellerId = detail.seller?.sellerId;
   const {
     profileImage,
     sellerId,
     sellerNickName,
     trustScore: rawScore,
   } = detail.seller || {};
+
+  const [showTrustTooltip, setShowTrustTooltip] = useState(false);
 
   const formatCount = (num) => {
     if (num < 1000) return num.toString();
@@ -64,23 +65,46 @@ const ProductSellerInfo = ({ detail }) => {
           </div>
 
           {/* 관심수 */}
-          <div className="flex flex-col items-center w-8" title="찜수">
+          <div className="flex flex-col items-center w-8 relative" title="찜수">
             <span className="text-lg font-semibold text-brand-green ">
               {wishCount}
-            </span>
+            </span>{" "}
             <span className="text-sm  text-brand-mediumgray">
               <Heart className="size-4" />
             </span>
           </div>
 
           {/* 신뢰점수 :) */}
-          <div className="flex flex-col items-center  w-8" title="신뢰점수">
+          <div className="flex flex-col items-center relative w-8 group">
             <span className="text-lg font-semibold text-brand-green">
               {trustScore}
             </span>
-            <span className=" text-brand-mediumgray">
+            <span
+              className="text-sm text-brand-mediumgray hover:text-brand-green transition-opacity duration-200 cursor-pointer"
+              onMouseEnter={() => setShowTrustTooltip(true)}
+              onMouseLeave={() => setShowTrustTooltip(false)}
+            >
               <Smile className="size-4" />
             </span>
+
+            {showTrustTooltip && (
+              <div className="absolute top-14 -right-1.5 w-48 p-3 bg-white border rounded-lg shadow-md z-50">
+                <div className="absolute -top-[6px] right-4 w-3 h-3 bg-white border-t border-l rotate-45 z-40"></div>
+                <div className="flex justify-between items-start mb-0.5 relative z-50">
+                  <div className="flex flex-col">
+                    <span className="text-xs font-bold text-brand-green">
+                      신뢰 점수란?
+                    </span>
+                    <span className="text-[10px] text-brand-mediumgray font-medium pt-0.5">
+                      최저 0 ~ 최고 5.0
+                    </span>
+                  </div>
+                </div>
+                <p className="text-[11px] text-brand-darkgray leading-relaxed relative z-50">
+                  거래 매너, 후기 등을 종합하여 계산된 판매자 신뢰 등급입니다.
+                </p>
+              </div>
+            )}
           </div>
         </div>
       </div>
